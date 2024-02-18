8a4b10b3823495b635d8bf0171fea06a2b5a9d8d
mirelois <79458822+mirelois@users.noreply.github.com>
2024-02-18 15:47:40 +0000

    tmux

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,12 +1,3 @@
-function Rose_Pine(trans)
-    vim.cmd.colorscheme("rose-pine")
-
-    if trans then
-        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    end
-end
-
 function Kanagawa(trans)
     -- Default options:
     require('kanagawa').setup({
@@ -31,21 +22,22 @@ function Kanagawa(trans)
                 }
             }
         },
+
         overrides = function(colors)
             local theme = colors.theme
             return {
-                NormalFloat = { bg = "none" },
-                FloatBorder = { bg = "none" },
-                -- Save an hlgroup with dark background and dimmed foreground
-                -- so that you can use it where your still want darker windows.
-                -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
-                NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-                -- Popular plugins that open floats will link to NormalFloat by default;
-                -- set their background accordingly if you wish to keep them dark and borderless
-                LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-                MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+                Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
+                PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+                PmenuSbar = { bg = theme.ui.bg_m1 },
+                PmenuThumb = { bg = theme.ui.bg_p2 },
+
+                TelescopeTitle = { fg = theme.ui.special, bold = true },
+                TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+                TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+                TelescopePreviewNormal = { bg = theme.ui.bg_dim },
             }
         end,
+
         theme = "wave", -- Load "wave" theme when 'background' option is not set
         background = {
             -- map the value of 'background' option to a theme
@@ -54,157 +46,11 @@ function Kanagawa(trans)
         },
     })
 
-    if trans then
-        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    end
+    -- if trans then
+    --     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    --     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    -- end
     vim.cmd.colorscheme("kanagawa")
 end
 
-function BOO(_theme, trans)
-    require("boo-colorscheme").use({ theme = _theme })
-
-    --themes--
-    --sunset_cloud
-    --radioactive_waste
-    --forest_stream
-    --crimson_moonlight
-
-    if trans then
-        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    end
-end
-
-function TokyoNight(_style, trans)
-    require("tokyonight").setup({ style = _style })
-
-    --styles--
-    --storm
-    --moon
-    --night
-    --day
-
-    vim.cmd.colorscheme("tokyonight")
-
-    if trans then
-        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    end
-end
-
-function GruvBox(trans)
-    vim.cmd.colorscheme("gruvbox")
-
-    if trans then
-        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    end
-end
-
-function Dracula(trans)
-    vim.cmd.colorscheme("dracula")
-
-    if trans then
-        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    end
-end
-
-function NightFox(_style, trans)
-    require('nightfox').setup({})
-    vim.cmd.colorscheme(_style .. "fox")
-
-    --themes--
-    --dawn
-    --night
-    --tera
-    --carbon
-    --nord
-    --dusk
-    --day
-
-    if trans then
-        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    end
-end
-
-function NotColorMyPencils()
-    vim.api.nvim_set_hl(0, "Normal", { bg = "black" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "black" })
-end
-
-function ColorMyPencils()
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
-
-function Catppuccin(trans)
-    local catppuccin = require("catppuccin")
-
-    catppuccin.setup({
-        -- latte, frappe, macchiato, mocha
-        transparent_background = trans,
-        flavour = "frappe"
-    })
-    --     integrations = {
-    --         ts_rainbow = true,
-    --     },
-    --     color_overrides = {
-    --         mocha = {
-    --             text = "#F4CDE9",
-    --             subtext1 = "#DEBAD4",
-    --             subtext0 = "#C8A6BE",
-    --             overlay2 = "#B293A8",
-    --             overlay1 = "#9C7F92",
-    --             overlay0 = "#866C7D",
-    --             surface2 = "#6E6A86", -- coments
-    --             surface1 = "#6E6A86", -- numerus
-    --             surface0 = "#44313B", --barra
-
-    --             base = "#352939",
-    --             mantle = "#211924",
-    --             crust = "#1a1016",
-    --         },
-    --     },
-    -- })
-
-    -- setup must be called before loading
-    vim.cmd.colorscheme "catppuccin"
-end
-
-function TokyoGogh(style, trans)
-    -- lua
-    require('tokyogogh').setup {
-        style = style, -- storm | night
-        term_colors = true,
-        -- Change code styles
-        code_styles = {
-            strings = { italic = false, bold = false },
-            comments = { italic = true, bold = false },
-            functions = { italic = false, bold = false },
-            variables = { italic = false, bold = false },
-        },
-        diagnostics = {
-            undercurl = true, -- use undercurl instead of underline
-            background = false,
-        },
-        -- Customization
-        colors = {},
-        highlight = {},
-    }
-    vim.cmd([[colorscheme tokyogogh]])
-    if trans then
-        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    end
-end
-
-
--- Catppuccin(true)
--- NightFox("dusk", true)
--- Dracula(true)
 Kanagawa(true)
--- TokyoNight(true)
--- TokyoGogh('night', true)
